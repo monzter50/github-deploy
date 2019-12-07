00650f6550f94936abae6a61e865b6770e69be8e
monster <josemonzter50@gmail.com>
2019-12-07 10:37:24 -0500

    Updates

---
+++ precache-manifest.131c4b25bbabd17da0e73fc336873004.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8da561c5df24ff8ab5f654594e17a875",
+    "url": "/github-deploy/index.html"
+  },
+  {
+    "revision": "d2de2dbba5a11115cf59",
+    "url": "/github-deploy/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5b499daaf87c7a07406e",
+    "url": "/github-deploy/static/js/2.fa81713b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/github-deploy/static/js/2.fa81713b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d2de2dbba5a11115cf59",
+    "url": "/github-deploy/static/js/main.c3bde5be.chunk.js"
+  },
+  {
+    "revision": "c4d958fcd2c88da1cebb",
+    "url": "/github-deploy/static/js/runtime-main.0bcf3644.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
